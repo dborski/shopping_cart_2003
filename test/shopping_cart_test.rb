463564f9b604e9ace97571da63001aecd2bac8d2
Derek Borski <derek.borski@gmail.com>
2020-03-25 09:30:23 -0600

    Initializes shopping cart class and tests attributes

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_instance_of ShoppingCart, cart
+  end
+
+  def test_it_has_a_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_equal "King Soopers", cart.name
+  end
+
+  def test_it_has_a_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_equal 30, cart.capacity
+  end
+
+end
