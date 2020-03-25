c7aa11457fcf991203ac1afe79319ea8c209ca4c
Derek Borski <derek.borski@gmail.com>
2020-03-25 09:15:18 -0600

    Fixes @unit_price and @quantity instance variables to better use arguments. Passes first 6 tests

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,15 @@
 class Product
 
-  attr_reader :category, :name
+  attr_reader :category,
+              :name,
+              :unit_price,
+              :quantity
+
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
-    @unit_price = 3.70
-    @quantity = quantity
+    @unit_price = unit_price
+    @quantity = quantity.to_i
   end
 
   def total_price
