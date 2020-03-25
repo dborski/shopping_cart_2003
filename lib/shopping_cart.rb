2c03b83f867f1d6ab14775b2893b4434438367a6
Derek Borski <derek.borski@gmail.com>
2020-03-25 10:13:02 -0600

    Adds products_by_category method and tests cart can be sorted by category

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -2,7 +2,8 @@ class ShoppingCart
 
   attr_reader :name,
               :capacity,
-              :products
+              :products,
+              :categories
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.to_i
@@ -23,6 +24,13 @@ class ShoppingCart
 
   def is_full?
     total_number_of_products > 30
+  end
+
+  def products_by_category(category)
+    sorted_categories = products.find_all do |product|
+       product.category == category
+    end
+    sorted_categories
   end
 
 
